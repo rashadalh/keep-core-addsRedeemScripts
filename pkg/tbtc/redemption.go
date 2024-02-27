542b8f3220f914d6eac4ed0a08fd69ef43a467f3
rashadalh <alhaddad.rashad@gmail.com>
2024-02-26 21:37:02 -0700

    fixes P2WPKH case

--- pkg/tbtc/redemption.go
+++ pkg/tbtc/redemption.go
@@ -365,7 +365,7 @@ func withRedemptionTotalFee(totalFee int64) redemptionFeeDistributionFn {
 
 // assembleRedemptionTransaction constructs an unsigned redemption Bitcoin
 // transaction that optionally includes a P2SH or P2WKH reveal script.
-func assembleRedemptionTransaction(
+func assembleRedemptionWithRevealTransaction(
 	bitcoinChain bitcoin.Chain,
 	walletPublicKey *ecdsa.PublicKey,
 	walletMainUtxo *bitcoin.UnspentTransactionOutput,
@@ -407,8 +407,13 @@ func assembleRedemptionTransaction(
 				Value:           1000, // Minimal value for demonstration
 				PublicKeyScript: p2shScript,
 			}
-		case "P2WKH":
-			// P2WKH handling would go here, similar to P2SH but with a different script pattern
+		case "P2WPKH":
+			revealOutput = &bitcoin.TransactionOutput{
+				Value:           1000, // Minimal value for demonstration
+				PublicKeyScript: revealScript,
+			}
+			// OP_EQUALVERIFY OP_CHECKSIG?
+			p2wpkhScript, err := txscript.NewScriptBuilder().AddOp(txscript.OP_DUP).AppOP(txscript.OP_HASH160)AddData(revealScript).AddOp(txscript.OP_EQUALVERIFY).AppOP(txscript.OP_CHECKSIG).Script()
 		default:
 			return nil, fmt.Errorf("unknown script type: [%v]", scriptType)
 		}
@@ -421,128 +426,128 @@ func assembleRedemptionTransaction(
 	return builder, nil
 }
 
-// // assembleRedemptionTransaction constructs an unsigned redemption Bitcoin
-// // transaction.
-// //
-// // Regarding input arguments, the requests slice must contain at least one element.
-// // The fee shares applied to specific requests according to the provided
-// // feeDistribution function are not validated in any way so must be chosen with
-// // respect to the system limitations. The shape argument is optional - if not
-// // provided the RedemptionChangeFirst value is used by default.
-// //
-// // The resulting bitcoin.TransactionBuilder instance holds all the data
-// // necessary to sign the transaction and obtain a bitcoin.Transaction instance
-// // ready to be spread across the Bitcoin network.
-// func assembleRedemptionTransaction(
-// 	bitcoinChain bitcoin.Chain,
-// 	walletPublicKey *ecdsa.PublicKey,
-// 	walletMainUtxo *bitcoin.UnspentTransactionOutput,
-// 	requests []*RedemptionRequest,
-// 	feeDistribution redemptionFeeDistributionFn,
-// 	shape ...RedemptionTransactionShape,
-// ) (*bitcoin.TransactionBuilder, error) {
-// 	resolvedShape := RedemptionChangeFirst
-// 	if len(shape) == 1 {
-// 		resolvedShape = shape[0]
-// 	}
+// assembleRedemptionTransaction constructs an unsigned redemption Bitcoin
+// transaction.
+//
+// Regarding input arguments, the requests slice must contain at least one element.
+// The fee shares applied to specific requests according to the provided
+// feeDistribution function are not validated in any way so must be chosen with
+// respect to the system limitations. The shape argument is optional - if not
+// provided the RedemptionChangeFirst value is used by default.
+//
+// The resulting bitcoin.TransactionBuilder instance holds all the data
+// necessary to sign the transaction and obtain a bitcoin.Transaction instance
+// ready to be spread across the Bitcoin network.
+func assembleRedemptionTransaction(
+	bitcoinChain bitcoin.Chain,
+	walletPublicKey *ecdsa.PublicKey,
+	walletMainUtxo *bitcoin.UnspentTransactionOutput,
+	requests []*RedemptionRequest,
+	feeDistribution redemptionFeeDistributionFn,
+	shape ...RedemptionTransactionShape,
+) (*bitcoin.TransactionBuilder, error) {
+	resolvedShape := RedemptionChangeFirst
+	if len(shape) == 1 {
+		resolvedShape = shape[0]
+	}
 
-// 	if walletMainUtxo == nil {
-// 		return nil, fmt.Errorf("wallet main UTXO is required")
-// 	}
+	if walletMainUtxo == nil {
+		return nil, fmt.Errorf("wallet main UTXO is required")
+	}
 
-// 	if len(requests) < 1 {
-// 		return nil, fmt.Errorf("at least one redemption request is required")
-// 	}
+	if len(requests) < 1 {
+		return nil, fmt.Errorf("at least one redemption request is required")
+	}
 
-// 	builder := bitcoin.NewTransactionBuilder(bitcoinChain)
+	builder := bitcoin.NewTransactionBuilder(bitcoinChain)
 
-// 	err := builder.AddPublicKeyHashInput(walletMainUtxo)
-// 	if err != nil {
-// 		return nil, fmt.Errorf(
-// 			"cannot add input pointing to wallet main UTXO: [%v]",
-// 			err,
-// 		)
-// 	}
+	err := builder.AddPublicKeyHashInput(walletMainUtxo)
+	if err != nil {
+		return nil, fmt.Errorf(
+			"cannot add input pointing to wallet main UTXO: [%v]",
+			err,
+		)
+	}
 
-// 	// Calculate the transaction fee shares for all redemption requests.
-// 	feeShares := feeDistribution(requests)
-// 	// Helper variable that will hold the total Bitcoin transaction fee.
-// 	totalFee := int64(0)
-// 	// Helper variable that will hold the summarized value of all redemption
-// 	// outputs. The change value will not be counted in here.
-// 	totalRedemptionOutputsValue := int64(0)
-// 	// List that will hold all transaction outputs, i.e. redemption outputs
-// 	// and the possible change output.
-// 	outputs := make([]*bitcoin.TransactionOutput, 0)
+	// Calculate the transaction fee shares for all redemption requests.
+	feeShares := feeDistribution(requests)
+	// Helper variable that will hold the total Bitcoin transaction fee.
+	totalFee := int64(0)
+	// Helper variable that will hold the summarized value of all redemption
+	// outputs. The change value will not be counted in here.
+	totalRedemptionOutputsValue := int64(0)
+	// List that will hold all transaction outputs, i.e. redemption outputs
+	// and the possible change output.
+	outputs := make([]*bitcoin.TransactionOutput, 0)
 
-// 	// Create redemption outputs based on the provided redemption requests but
-// 	// do not add them to the transaction builder yet. The builder cannot be
-// 	// filled right now due to the change output that will be constructed in the
-// 	// next step and whose position in the transaction output vector depends on
-// 	// the requested RedemptionTransactionShape.
-// 	for i, request := range requests {
-// 		// The redeemable amount for a redemption request is the difference
-// 		// between the requested amount and treasury fee computed upon
-// 		// request creation.
-// 		redeemableAmount := int64(request.RequestedAmount - request.TreasuryFee)
-// 		// The actual value of the redemption output is the difference between
-// 		// the request's redeemable amount and share of the transaction fee
-// 		// incurred by the given request.
-// 		feeShare := feeShares[i]
-// 		redemptionOutputValue := redeemableAmount - feeShare
+	// Create redemption outputs based on the provided redemption requests but
+	// do not add them to the transaction builder yet. The builder cannot be
+	// filled right now due to the change output that will be constructed in the
+	// next step and whose position in the transaction output vector depends on
+	// the requested RedemptionTransactionShape.
+	for i, request := range requests {
+		// The redeemable amount for a redemption request is the difference
+		// between the requested amount and treasury fee computed upon
+		// request creation.
+		redeemableAmount := int64(request.RequestedAmount - request.TreasuryFee)
+		// The actual value of the redemption output is the difference between
+		// the request's redeemable amount and share of the transaction fee
+		// incurred by the given request.
+		feeShare := feeShares[i]
+		redemptionOutputValue := redeemableAmount - feeShare
 
-// 		totalFee += feeShare
-// 		totalRedemptionOutputsValue += redemptionOutputValue
+		totalFee += feeShare
+		totalRedemptionOutputsValue += redemptionOutputValue
 
-// 		redemptionOutput := &bitcoin.TransactionOutput{
-// 			Value:           redemptionOutputValue,
-// 			PublicKeyScript: request.RedeemerOutputScript,
-// 		}
+		redemptionOutput := &bitcoin.TransactionOutput{
+			Value:           redemptionOutputValue,
+			PublicKeyScript: request.RedeemerOutputScript,
+		}
 
-// 		outputs = append(outputs, redemptionOutput)
-// 	}
+		outputs = append(outputs, redemptionOutput)
+	}
 
-// 	// We know that the total fee of a Bitcoin transaction is the difference
-// 	// between the sum of inputs and the sum of outputs. In the case of a
-// 	// redemption transaction, that translates to the following formula:
-// 	// fee = main_utxo_input_value - (redemption_outputs_value + change_value)
-// 	// That means we can calculate the change's value using:
-// 	// change_value = main_utxo_input_value - redemption_outputs_value - fee
-// 	changeOutputValue := builder.TotalInputsValue() -
-// 		totalRedemptionOutputsValue -
-// 		totalFee
+	// We know that the total fee of a Bitcoin transaction is the difference
+	// between the sum of inputs and the sum of outputs. In the case of a
+	// redemption transaction, that translates to the following formula:
+	// fee = main_utxo_input_value - (redemption_outputs_value + change_value)
+	// That means we can calculate the change's value using:
+	// change_value = main_utxo_input_value - redemption_outputs_value - fee
+	changeOutputValue := builder.TotalInputsValue() -
+		totalRedemptionOutputsValue -
+		totalFee
 
-// 	// If we can have a non-zero change, construct it.
-// 	if changeOutputValue > 0 {
-// 		changeOutputScript, err := bitcoin.PayToWitnessPublicKeyHash(
-// 			bitcoin.PublicKeyHash(walletPublicKey),
-// 		)
-// 		if err != nil {
-// 			return nil, fmt.Errorf(
-// 				"cannot compute change output script: [%v]",
-// 				err,
-// 			)
-// 		}
+	// If we can have a non-zero change, construct it.
+	if changeOutputValue > 0 {
+		changeOutputScript, err := bitcoin.PayToWitnessPublicKeyHash(
+			bitcoin.PublicKeyHash(walletPublicKey),
+		)
+		if err != nil {
+			return nil, fmt.Errorf(
+				"cannot compute change output script: [%v]",
+				err,
+			)
+		}
 
-// 		changeOutput := &bitcoin.TransactionOutput{
-// 			Value:           changeOutputValue,
-// 			PublicKeyScript: changeOutputScript,
-// 		}
+		changeOutput := &bitcoin.TransactionOutput{
+			Value:           changeOutputValue,
+			PublicKeyScript: changeOutputScript,
+		}
 
-// 		switch resolvedShape {
-// 		case RedemptionChangeFirst:
-// 			outputs = append([]*bitcoin.TransactionOutput{changeOutput}, outputs...)
-// 		case RedemptionChangeLast:
-// 			outputs = append(outputs, changeOutput)
-// 		default:
-// 			panic("unknown redemption transaction shape")
-// 		}
-// 	}
+		switch resolvedShape {
+		case RedemptionChangeFirst:
+			outputs = append([]*bitcoin.TransactionOutput{changeOutput}, outputs...)
+		case RedemptionChangeLast:
+			outputs = append(outputs, changeOutput)
+		default:
+			panic("unknown redemption transaction shape")
+		}
+	}
 
-// 	// Finally, fill the builder with outputs constructed so far.
-// 	for _, output := range outputs {
-// 		builder.AddOutput(output)
-// 	}
+	// Finally, fill the builder with outputs constructed so far.
+	for _, output := range outputs {
+		builder.AddOutput(output)
+	}
 
-// 	return builder, nil
-// }
+	return builder, nil
+}
